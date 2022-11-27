0ad7dbe0fd5bcdfea8edafff91703843dc499d69
alimert1 <yilmazalimert20@gmail.com>
2022-11-27 22:41:41 +0300

    loginpage

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -2,8 +2,33 @@ import "./login.scss"
 
 const Login = () => {
     return (
-        <div className = "login">Login</div>
-    )
-}
+        <div className = "login">
+            <div className="card">
+                <div className="left">
+                    <h1>Hello world.</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Corporis ex soluta voluptatibus reprehenderit est beatae 
+                        nobis saepe sapiente omnis officiis.
+                    </p>
+                    <span>Don't you have an account?</span>
+                    <button>Register</button>
+
+                </div>
+
+
+                <div className="right">
+                    <h1>Login</h1>
+                    <form>
+                        <input type="text" placeholder="Username" />
+                        <input type="password" placeholder="Password" />
+                        <button>Login</button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Login
